37dce3fd6cb603997e9e39bcb59df4be726a60dc
Olivier Van hamme <nicefiction@gmail.com>
2021-08-04 11:21:12 +0200

    Initial Commit

---
+++ Friendface/FriendfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendfaceApp.swift
+//  Friendface
+//
+//  Created by Olivier Van hamme on 04/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct FriendfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
